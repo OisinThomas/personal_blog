c9691198b787e43fd28166356f2571b149cf1d62
Oisín Thomas Morrin <oisin.thomas.morrin@gmail.com>
2023-10-21 15:06:18 +0100

    Initial website

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+import { parseISO, compareDesc } from "date-fns";
+
+export const cx = (...classNames) => classNames.filter(Boolean).join(" ");
+
+export const sortBlogs = (blogs) => {
+  return blogs.sort((a, b) => {
+    return compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt));
+  });
+};
