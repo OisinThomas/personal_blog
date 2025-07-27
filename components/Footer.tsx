3db015980963074417e2209b0d17fe1ceed25e08
OisinThomas <oisin.thomas.morrin@gmail.com>
2025-07-27 23:08:46 +0100

    fix: update footer copyright year to 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 export default function Footer(props: any) {
   return (
     <footer className={clsx("flex justify-between items-center p-4 bg-gray-100 dark:bg-gray-700 py-8 mt-16", props)}>
-      <div className="text-sm">© Oisín Thomas</div>
+      <div className="text-sm">© Oisín Thomas 2025</div>
       <Socials/>
     </footer>
   );
